e8de68a0ff3f6083bbf45b84a1918a3106a2a5f0
antziko <antzyap@hotmail.com>
2025-04-15 20:32:23 +0800

    Initial commit

--- main.ts
+++ main.ts
@@ -1,134 +1,89 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin } from "obsidian";
+import { EditorView, ViewPlugin, ViewUpdate, Decoration, DecorationSet } from "@codemirror/view";
+import { RangeSetBuilder } from "@codemirror/state";
 
-// Remember to rename these classes and interfaces!
+// Shared tone map logic
+const toneMap: Record<string, string> = {
+  'ā': 'tone1', 'ē': 'tone1', 'ī': 'tone1', 'ō': 'tone1', 'ū': 'tone1', 'ǖ': 'tone1',
+  'á': 'tone2', 'é': 'tone2', 'í': 'tone2', 'ó': 'tone2', 'ú': 'tone2', 'ǘ': 'tone2',
+  'ǎ': 'tone3', 'ě': 'tone3', 'ǐ': 'tone3', 'ǒ': 'tone3', 'ǔ': 'tone3', 'ǚ': 'tone3',
+  'à': 'tone4', 'è': 'tone4', 'ì': 'tone4', 'ò': 'tone4', 'ù': 'tone4', 'ǜ': 'tone4'
+};
 
-interface MyPluginSettings {
-	mySetting: string;
+const toneChars = Object.keys(toneMap).join('');
+const regex = new RegExp(`[\\w${toneChars}]*[\\w${toneChars}]+`, 'g');
+
+function getToneClass(word: string): string | null {
+  for (const c of word) {
+    if (toneMap[c]) return toneMap[c];
+  }
+  return null;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export default class PinyinHighlighterPlugin extends Plugin {
+  async onload() {
+    // 🖊️ Live Preview (Editor Mode)
+    const pinyinExtension = ViewPlugin.fromClass(class {
+      decorations: DecorationSet;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+      constructor(view: EditorView) {
+        this.decorations = this.buildDecorations(view);
+      }
 
-	async onload() {
-		await this.loadSettings();
+      update(update: ViewUpdate) {
+        if (update.docChanged || update.viewportChanged) {
+          this.decorations = this.buildDecorations(update.view);
+        }
+      }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+      buildDecorations(view: EditorView): DecorationSet {
+        const builder = new RangeSetBuilder<Decoration>();
+        for (let { from, to } of view.visibleRanges) {
+          const text = view.state.doc.sliceString(from, to);
+          let match: RegExpExecArray | null;
+          while ((match = regex.exec(text)) !== null) {
+            const [word] = match;
+            const start = from + match.index;
+            const end = start + word.length;
+            const toneClass = getToneClass(word);
+            if (toneClass) {
+              builder.add(start, end, Decoration.mark({ class: `pinyin-editor-${toneClass}` }));
+            }
+          }
+        }
+        return builder.finish();
+      }
+    }, {
+      decorations: v => v.decorations
+    });
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    this.registerEditorExtension(pinyinExtension);
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    // 📖 Read Mode (Preview)
+    this.registerMarkdownPostProcessor((el) => {
+      function walk(node: Node) {
+        if (node.nodeType === 3) {
+          const text = node.nodeValue!;
+          const replaced = text.replace(regex, (word) => {
+            const toneClass = getToneClass(word);
+            return toneClass
+              ? `<span class="pinyin-preview-${toneClass}">${word}</span>`
+              : word;
+          });
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+          if (replaced !== text) {
+            const span = document.createElement('span');
+            span.innerHTML = replaced;
+            if (node.parentNode) {
+                node.parentNode.replaceChild(span, node);
+            }
+          }
+        } else if (node.nodeType === 1 && !(node instanceof HTMLScriptElement || node instanceof HTMLStyleElement)) {
+          Array.from(node.childNodes).forEach(walk);
+        }
+      }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+      walk(el);
+    });
+  }
 }
